5baefc7d91d12108ddc33de01e418f3aaab2a028
Filip Mårtensson <fenwikk@protonmail.com>
2024-04-13 22:15:43 +0200

    switch to svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Header from '$lib/components/Header.svelte';
+</script>
+
+<Header animate heading="Tjabbahallå!" subheading="developer - göteborg" />
+
+<main>
+	<div id="about">
+		<h2>About</h2>
+	</div>
+</main>
+
+<style>
+</style>
