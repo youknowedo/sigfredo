68d1c4c421bc1a44dff585325430e4529e54cb5a
Filip Mårtensson <fenwikk@protonmail.com>
2024-08-10 15:41:20 +0200

    added sprigkit to projects

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -150,23 +150,44 @@
 		</div>
 
 		<div class="relative flex h-[4.1in] justify-center">
-			<a
-				href="https://chromewebstore.google.com/detail/yaffe/beoeheggmopaddkfhcpjfllbmkmofmgg"
-				target="_blank"
-				class="absolute top-0 z-50 flex rotate-3 justify-center drop-shadow-md duration-200 hover:-top-4 hover:rotate-0 hover:pb-6 focus:-top-4 focus:rotate-0 focus:pb-6"
-			>
-				<span class="hidden">Yet Another Fast-Forward Extension</span>
-				<div class="flex aspect-[3.5/4.2] h-[4.2in] w-[3.5in] flex-col items-center bg-white">
-					<img
-						class="mx-auto mt-[0.2in] h-[3.1in] w-[3.1in]"
-						src="/assets/projects/yaffe.webp"
-						alt=""
-					/>
-					<div class="my-auto">
-						<img class="h-14" src="/assets/projects/yaffe-text.webp" alt="" />
+			<div class="w-[3.5in]">
+				<a
+					href="https://chromewebstore.google.com/detail/yaffe/beoeheggmopaddkfhcpjfllbmkmofmgg"
+					target="_blank"
+					class="absolute top-0 z-50 flex rotate-3 justify-center drop-shadow-md duration-200 hover:-top-4 hover:rotate-0 hover:pb-6 focus:-top-4 focus:rotate-0 focus:pb-6"
+				>
+					<span class="hidden">Yet Another Fast-Forward Extension</span>
+					<div class="flex aspect-[3.5/4.2] h-[4.2in] w-[3.5in] flex-col items-center bg-white">
+						<img
+							class="mx-auto mt-[0.2in] h-[3.1in] w-[3.1in]"
+							src="/assets/projects/yaffe.webp"
+							alt=""
+						/>
+						<div class="my-auto">
+							<img class="h-14" src="/assets/projects/yaffe-text.webp" alt="" />
+						</div>
 					</div>
-				</div>
-			</a>
+				</a>
+			</div>
+			<div class="w-[3.5in]">
+				<a
+					href="https://github.com/youknowedo/sprigkit/"
+					target="_blank"
+					class="absolute -top-1 z-50 flex -rotate-2 justify-center drop-shadow-md duration-200 hover:-top-4 hover:rotate-0 hover:pb-6 focus:-top-4 focus:rotate-0 focus:pb-6"
+				>
+					<span class="hidden">SprigKit</span>
+					<div class="flex aspect-[3.5/4.2] h-[4.2in] w-[3.5in] flex-col items-center bg-white">
+						<img
+							class="mx-auto mt-[0.2in] h-[3.1in] w-[3.1in]"
+							src="/assets/projects/sprigkit.webp"
+							alt=""
+						/>
+						<div class="my-auto">
+							<img class="h-14" src="/assets/projects/sprigkit-text.webp" alt="" />
+						</div>
+					</div>
+				</a>
+			</div>
 		</div>
 	</div>
 
