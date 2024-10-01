7f41d5a21334b63d4ecf6f568974ca41a0da390d
Filip Mårtensson <fenwikk@protonmail.com>
2024-10-01 19:37:44 +0200

    return butotn styles

--- packages/ui/src/lib/Layout.svelte
+++ packages/ui/src/lib/Layout.svelte
@@ -5,20 +5,27 @@
 	export let shy = true;
 </script>
 
-<button
-	class="{sticky
-		? 'fixed'
-		: 'absolute'} inset-x-0 flex items-center justify-center duration-300 {shy
-		? '-top-24 hover:top-4'
-		: 'top-4'}"
->
-	<img class="absolute w-[1536px] object-fill" src={rip} alt="" />
+<div class="relative">
+	<a
+		href="https://sigfredo.fun"
+		class="{sticky
+			? 'fixed'
+			: 'absolute'} inset-x-0 z-50 flex items-center justify-center overflow-visible duration-300 {shy
+			? '-top-24 hover:top-4'
+			: 'top-4'}"
+	>
+		<div class="absolute z-40 w-[1024px]">
+			<img class="w-[1024px]" src={rip} alt="" />
+		</div>
 
-	<div class="font-typeWriter z-50 -mb-10 flex items-center gap-2 text-black">
-		<img class="mb-1 h-6 w-6" src="/ico.webp" alt="" />
-		Go back to
-		<a href="https://sigfredo.fun">sigfredo.fun</a>
+		<div class="z-50 -mb-10 flex items-center gap-2 font-typeWriter text-black">
+			<img class="mb-1 h-6 w-6" src="/ico.webp" alt="" />
+			Go back to
+			<a href="https://sigfredo.fun">sigfredo.fun</a>
+		</div>
+	</a>
+
+	<div class="z-20">
+		<slot />
 	</div>
-</button>
-
-<slot />
+</div>
